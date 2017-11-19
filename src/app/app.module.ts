b004f34c6a389b5c763402af13ff7674e0d586f1
Brandon Whisnant <bc.whisnant@gmail.com>
2017-11-19 11:19:54 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderSectionComponent } from './header-section/header-section.component';
+import { MiddleSectionComponent } from './middle-section/middle-section.component';
+import { BottomSectionComponent } from './bottom-section/bottom-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderSectionComponent,
+    MiddleSectionComponent,
+    BottomSectionComponent
   ],
   imports: [
     BrowserModule
